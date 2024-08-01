90ff3ccc57a9f190ab654279f93c511f28597ed0
Aydimir Aydimirov <aydimir95@gmail.com>
2024-08-01 22:36:08 +0300

    5

---
+++ CS50 - Python/Week_1_Conditionals/3_parity.py
@@ -0,0 +1,64 @@
+"""
+
++
+-
+*
+/
+% - remainder
+
+"""
+
+# Is the number EVEN or ODD
+
+x = int(input("What's x? "))
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Make it better
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
+
+# Even better way = Pythonic way to solve this 
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+main()
+
+# Even better way
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+
+def is_even(n):
+    return n % 2 == 0 # The most elegant way to present this idea
+
+main()
---
+++ CS50 - Python/Week_1_Conditionals/scrimba.py
@@ -0,0 +1,9 @@
+item_1 = "tomato"
+price_1 = 30
+inventory_1 = 2
+is_in_inventory = True
+
+print(item_1 + " = " + str(price_1) + "$" + " @ " + str(inventory_1))
+
+print(3//2)
+
