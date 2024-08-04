b1825b431db5563bb4b04084b6e8e3ffd8cc8acd
Aydimir95 <aydimir95@gmail.com>
2024-08-04 22:42:57 +0300

    Week 4 - library

---
+++ CS50 - Python/Week_4_Libraries/1_generate.py
@@ -0,0 +1,52 @@
+# modules - removes duplicating functions again and again - make it reusable
+
+# random - library
+
+# import - allows to import the functions from library
+# random.choice(seq) 
+
+# coin toss to generate the random number
+
+
+'''
+import random
+
+
+coin = random.choice(["heads", "tails"])
+print(coin)
+
+
+
+# from - importing functions from module
+
+from random import choice
+
+coin = choice(["heads", "tails"])
+print(coin)
+
+
+# random.randint(a, b) - the probability between the 2 numbers 
+
+import random
+
+number = random.randint(1, 10)
+print(number)
+
+
+
+
+# random.shuffle(x)
+
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
+
+
+'''
+
+
+import random
+
