ef46c8e224928954e30486502445ff0b64e2c6fe
Aydimir Aydimirov <aydimir95@gmail.com>
2024-08-07 22:20:05 +0300

    del

--- CS50 - Python - Copy/Week_1_Conditionals/freecodecamp.py
+++
@@ -1,9 +0,0 @@
-text = 'Hello World'
-#print(text[6])
-#print(len(text))
-print(type(text))
-
-
-shift = 3
-print(type(shift))
-
--- CS50 - Python - Copy/Week_1_Conditionals/scrimba.py
+++
@@ -1,9 +0,0 @@
-item_1 = "tomato"
-price_1 = 30
-inventory_1 = 2
-is_in_inventory = True
-
-print(item_1 + " = " + str(price_1) + "$" + " @ " + str(inventory_1))
-
-print(3//2)
-
