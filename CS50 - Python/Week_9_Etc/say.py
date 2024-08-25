29fb7201fd82898ad17915e984537dac837fb316
Aydimir95 <aydimir95@gmail.com>
2024-08-25 15:29:06 +0300

    CS50 Python Course Completed - Cowsay works now

--- CS50 - Python/Week_9_Etc/say.py
+++ CS50 - Python/Week_9_Etc/say.py
@@ -1,3 +1,4 @@
+"""
 import cowsay
 import pyttsx3
 
@@ -6,3 +7,15 @@ this = input("What's this? ")
 cowsay.cow(this)
 engine.say(this)
 engine.runAndWait()
+"""
+
+from gtts import gTTS
+import os
+import cowsay
+
+this = input("What's this? ")
+cowsay.cow(this)
+
+tts = gTTS(text=this, lang="en")
+tts.save("output.mp3")
+os.system("mpg321 output.mp3")
